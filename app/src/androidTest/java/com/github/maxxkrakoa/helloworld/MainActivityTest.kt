60dba750d48372bb07ec0e856e11bd07a342baee
Mads Villadsen <maxx@krakoa.dk>
2018-12-20 11:31:55 +0100

    Switch to com.android.support.test to make tests work in App Center

--- app/src/androidTest/java/com/github/maxxkrakoa/helloworld/MainActivityTest.kt
+++ app/src/androidTest/java/com/github/maxxkrakoa/helloworld/MainActivityTest.kt
@@ -1,22 +1,33 @@
 package com.github.maxxkrakoa.helloworld
 
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.LargeTest
-import androidx.test.rule.ActivityTestRule
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import com.microsoft.appcenter.espresso.Factory;
+import com.microsoft.appcenter.espresso.ReportHelper;
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.filters.LargeTest
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import org.junit.After
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class MainActivityTest {
     @get:Rule
     val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @get:Rule
+    val reportHelper = Factory.getReportHelper() as ReportHelper
+
+    @After
+    fun TearDown() {
+        reportHelper.label("Stopping App")
+    }
 
     @Test fun buttonWorks() {
         onView(withId(R.id.textView)).check(matches(withText("Hello World!")))
